a9827e0024b49d8c903a922ca791734dd8ec173c
EnzoBenechIUT <enzo.benech@etu.univ-smb.fr>
2025-11-13 09:09:36 +0100

    Creation of ProductCategory

---
+++ Astralis_API/Models/EntityFramework/Product/ProductCategory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Astralis_API.Models.EntityFramework
+{
+    [Table("t_e_productcategory_prc")]
+    public class ProductCategory
+    {
+        [Key]
+        [Column("prc_id")]
+        public int Id { get; set; }
+
+        [Column("prc_label")]
+        [Required(ErrorMessage = "The product category label is required.")]
+        [StringLength(100, ErrorMessage = "The product category label length must not be over 100 characters.")]
+        public String Label { get; set; }
+
+        [Column("prc_description")]
+        [StringLength(300, ErrorMessage = "The product category description length must not be over 300 characters.")]
+        public String Description { get; set; }
+    }
+}
