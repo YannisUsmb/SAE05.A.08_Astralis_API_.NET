08526e57afe8befc0abe6db9914f590f4dcd2775
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-14 16:03:23 +0100

    Added Cart Model.

---
+++ Astralis_API/Models/EntityFramework/Products/Cart.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Astralis_API.Models.EntityFramework
+{
+    [Table("t_j_cart_crt")]
+    public class Cart
+    {
+        [Column("usr_id")]
+        [Required(ErrorMessage = "The user ID is required.")]
+        public int UserId { get; set; }
+
+        [Column("pro_id")]
+        [Required(ErrorMessage = "The product ID is required.")]
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual User User { get; set; } = null!;
+
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; } = null!;
+    }
+}
