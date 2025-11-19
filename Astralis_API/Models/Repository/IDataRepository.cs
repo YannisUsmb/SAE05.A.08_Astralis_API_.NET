59f833953475716398e3bf263cc46c5abe4594fa
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-19 08:37:21 +0100

    Added IDataRepository.

---
+++ Astralis_API/Models/Repository/IDataRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Astralis_API.Models.Repository
+{
+    public interface IDataRepository<TEntity, TIdentifier, TKey> : IReadableRepository<TEntity, TIdentifier>, IWriteableRepository<TEntity>, ISearchRepository<TEntity, TKey>
+    {
+    }
+}
