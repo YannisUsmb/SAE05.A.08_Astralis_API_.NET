1cd0b2a30e276f986a15aeed69a648f342d68ef8
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-19 08:34:23 +0100

    Added IWriteableRepository.

---
+++ Astralis_API/Models/Repository/IWriteableRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Astralis_API.Models.Repository
+{
+    public interface IWriteableRepository<TEntity>
+    {
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
