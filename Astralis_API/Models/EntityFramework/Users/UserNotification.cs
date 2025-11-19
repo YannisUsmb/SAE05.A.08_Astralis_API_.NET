47fa1c709cdeb4c0a8fb348b46bb254b11abd9af
EnzoBenechIUT <enzo.benech@etu.univ-smb.fr>
2025-11-19 08:51:39 +0100

    Add of Notification and UserNotification classes to the user mangagement

---
+++ Astralis_API/Models/EntityFramework/Users/UserNotification.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Astralis_API.Models.EntityFramework
+{
+    [Table("t_j_usernotification_uno")]
+    public class UserNotification
+    {
+        [Key]
+        [Column("usr_id")]
+        public int UserId { get; set; }
+
+        [Key]
+        [Column("not_id")]
+        public int NotificationId { get; set; }
+
+        [Column("uno_bymail")]
+        [Required(ErrorMessage = "The mail user notification status is required.")]
+        public bool ByMail { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty(nameof(User.UserNotifications))]
+        public virtual User UserNavigation { get; set; } = null!;
+
+        [ForeignKey(nameof(NotificationId))]
+        [InverseProperty(nameof(Notification.UserNotifications))]
+        public virtual Notification NotificationNavigation { get; set; } = null!;
+
+
+    }
+}
