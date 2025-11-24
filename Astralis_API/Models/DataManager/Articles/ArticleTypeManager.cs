8433c3be9ad3ff91b9314a7d9c1c286f06ca95be
EnzoBenechIUT <enzo.benech@etu.univ-smb.fr>
2025-11-24 11:32:19 +0100

    fix datamanager of article type

--- Astralis_API/Models/DataManager/Articles/ArticleTypeManager.cs
+++ Astralis_API/Models/DataManager/Articles/ArticleTypeManager.cs
@@ -4,24 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Astralis_API.Models.DataManager
 {
-    public class ArticleTypeManager : DataManager<Article, int, string>, ISearchRepository<Article, string>
+    public class ArticleTypeTypeManager : DataManager<ArticleType, int, string>
     {
-        private readonly AstralisDbContext? _context;
-        private readonly DbSet<Article> _articles;
-
-        public ArticleTypeManager(AstralisDbContext context) : base(context)
+        public ArticleTypeTypeManager(AstralisDbContext context) : base(context)
         {
-            _context = context;
-            _articles = _context.Set<Article>();
-        }
-
-        public async Task<IEnumerable<Article>> GetByKeyAsync(string title)
-        {
-            return await _articles.Where(cb => cb.Title.ToLower().Contains(title.ToLower()))
-                            .Include(cb => cb.TypesOfArticle)
-                            .Include(cb=> cb.ArticleInterests)
-                            .Include(cb=> cb.UserNavigation)
-                            .ToListAsync();
-        }
+        }        
     }
 }
