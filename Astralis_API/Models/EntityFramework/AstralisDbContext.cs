216c09bdb4312cc547a62df6860fb5d303340b8e
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-11-24 08:32:36 +0100

    onmodelcreating beggining

--- Astralis_API/Models/EntityFramework/AstralisDbContext.cs
+++ Astralis_API/Models/EntityFramework/AstralisDbContext.cs
@@ -13,14 +13,19 @@ public partial class AstralisDbContext : DbContext
     {
     }
 
+    // Adresses
     public virtual DbSet<Address> Addresses { get; set; }
     public virtual DbSet<City> Cities { get; set; }
     public virtual DbSet<Country> Countries { get; set; }
     public virtual DbSet<PhonePrefix> PhonePrefixes { get; set; }
+
+    // Articles
     public virtual DbSet<Article> Articles { get; set; }
     public virtual DbSet<ArticleInterest> ArticleInterests { get; set; }
     public virtual DbSet<ArticleType> ArticleTypes { get; set; }
     public virtual DbSet<TypeOfArticle> TypesOfArticle { get; set; }
+
+    // Celestial Bodies 
     public virtual DbSet<OrbitalClass> OrbitalClasses { get; set; }
     public virtual DbSet<Asteroid> Asteroids { get; set; }
     public virtual DbSet<Audio> Audios { get; set; }
@@ -38,19 +43,29 @@ public partial class AstralisDbContext : DbContext
     public virtual DbSet<Satellite> Satellites { get; set; }
     public virtual DbSet<SpectralClass> SpectralClasses { get; set; }
     public virtual DbSet<Star> Stars { get; set; }
+
+    // Comments
     public virtual DbSet<Comment> Comments { get; set; }
+
+    // Events
     public virtual DbSet<Event> Events { get; set; }
     public virtual DbSet<EventInterest> EventInterests { get; set; }
     public virtual DbSet<EventType> EventTypes { get; set; }
+
+    // Products
     public virtual DbSet<CartItem> CartItems { get; set; }
     public virtual DbSet<Command> Commands { get; set; }
     public virtual DbSet<CommandStatus> CommandStatuses { get; set; }
     public virtual DbSet<OrderDetail> OrderDetails { get; set; }
     public virtual DbSet<Product> Products { get; set; }
     public virtual DbSet<ProductCategory> ProductCategories { get; set; }
+
+    // Reports
     public virtual DbSet<Report> Reports { get; set; }
     public virtual DbSet<ReportMotive> ReportMotives { get; set; }
     public virtual DbSet<ReportStatus> ReportStatuses { get; set; }
+
+    // Users
     public virtual DbSet<CreditCard> CreditCards { get; set; }
     public virtual DbSet<Notification> Notifications { get; set; }
     public virtual DbSet<User> Users { get; set; }
@@ -64,6 +79,17 @@ public partial class AstralisDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         OnModelCreatingPartial(modelBuilder);
+        modelBuilder.HasDefaultSchema("public");
+
+        modelBuilder.Entity<Address>(entity =>
+        {
+            entity.HasKey(a => a.Id).HasName("adress_pkey");
+
+            entity.HasOne(a => a.CityNavigation)
+                .WithMany(c => c.Addresses)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_");
+        });
     }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
