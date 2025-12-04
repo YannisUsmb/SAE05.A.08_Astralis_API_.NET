d500022359adfb17f912fcee8339d6fafa5659ab
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 09:04:04 +0100

    fix(model) : fixed model notificationtype and made a migration to update the db

--- Astralis_API/Migrations/20251204075959_FixedNotificationTypeTableNameMigration.Designer.cs
+++ Astralis_API/Migrations/20251204075959_FixedNotificationTypeTableNameMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Astralis_API.Models.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Astralis_API.Migrations
 {
     [DbContext(typeof(AstralisDbContext))]
-    partial class AstralisDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251204075959_FixedNotificationTypeTableNameMigration")]
+    partial class FixedNotificationTypeTableNameMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Astralis_API/Migrations/20251204075959_FixedNotificationTypeTableNameMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Astralis_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedNotificationTypeTableNameMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "t_e_notification_nty",
+                schema: "public",
+                newName: "t_e_notificationtype_nty",
+                newSchema: "public");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "t_e_notificationtype_nty",
+                schema: "public",
+                newName: "t_e_notification_nty",
+                newSchema: "public");
+        }
+    }
+}
