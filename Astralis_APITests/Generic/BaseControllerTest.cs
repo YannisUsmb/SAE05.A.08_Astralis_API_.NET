857d8612b26510851374f6f2813fe80b18a956b0
EnzoBenechIUT <enzo.benech@etu.univ-smb.fr>
2025-12-08 16:12:25 +0100

    Start for the tests of the API

---
+++ Astralis_APITests/Generic/BaseControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using Astralis_API.Models.EntityFramework;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Astralis_API.Tests.Controllers
+{
+    public abstract class BaseControllerTests<TEntity, TController>
+        where TEntity : class, new()
+        where TController : class
+    {
+        protected AstralisDbContext _context;
+        protected IMapper _mapper;
+        protected TController _controller;
+
+        protected abstract TController CreateController(AstralisDbContext context, IMapper mapper);
+        protected abstract List<TEntity> GetSampleEntities();
+
+        [TestInitialize]
+        public virtual void BaseInitialize()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+            var testConnectionString = configuration.GetConnectionString("DefaultConnection");
+            var options = new DbContextOptionsBuilder<AstralisDbContext>()
+                .UseNpgsql(testConnectionString)
+                .Options;
+
+            _context = new AstralisDbContext(options);
+
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new Models.Mapper.MapperProfile());
+            });
+            _mapper = config.CreateMapper();
+            var samples = GetSampleEntities();
+            _context.Set<TEntity>().AddRange(samples);
+            _context.SaveChanges();
+            _controller = CreateController(_context, _mapper);
+        }
+
+        [TestCleanup]
+        public virtual void BaseCleanup()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+    }
+}
---
+++ Astralis_APITests/Generic/ReadableControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Astralis_API.Tests.Controllers
+{
+    public abstract class ReadableControllerTests<TEntity, TController, TGetAllDto, TGetDto, TId>
+        : BaseControllerTests<TEntity, TController>
+        where TEntity : class, new()
+        where TController : class
+        where TGetAllDto : class
+        where TGetDto : class
+    {
+        protected abstract TId GetEntityId(TEntity entity);
+
+        protected abstract Task<ActionResult<IEnumerable<TGetAllDto>>> ActionGetAll();
+        protected abstract Task<ActionResult<TGetDto>> ActionGetById(TId id);
+
+        [TestMethod]
+        public async Task Readable_GetAll_ShouldReturnOk()
+        {
+            var result = await ActionGetAll();
+
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsNotNull(okResult, "Le résultat n'est pas un OkObjectResult (200).");
+
+            var items = okResult.Value as IEnumerable<TGetAllDto>;
+            Assert.IsNotNull(items, "Le contenu est null.");
+            Assert.AreEqual(GetSampleEntities().Count, items.Count(), "Le nombre d'éléments retournés est incorrect.");
+        }
+
+        [TestMethod]
+        public async Task Readable_GetById_ShouldReturnItem()
+        {
+            var entity = _context.Set<TEntity>().First();
+            var id = GetEntityId(entity);
+
+            var result = await ActionGetById(id);
+
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsNotNull(okResult, "Le résultat n'est pas un OkObjectResult (200).");
+            Assert.IsInstanceOfType(okResult.Value, typeof(TGetDto));
+        }
+    }
+}
