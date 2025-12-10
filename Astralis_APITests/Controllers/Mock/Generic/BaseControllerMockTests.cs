c7eee20167ba2828677cf8a8dbd925e3fea6925a
YannisUsmb <Yannis.Mauline@etu.univ-smb.fr>
2025-12-10 09:48:21 +0100

    feat(tests) : begining of the mock tests

---
+++ Astralis_APITests/Controllers/Mock/Generic/BaseControllerMockTests.cs
@@ -0,0 +1,20 @@
+﻿using Astralis_API.Models.Mapper;
+using AutoMapper;
+
+namespace Astralis_APITests.Controllers
+{
+    public abstract class BaseControllerMockTests
+    {
+        protected IMapper _mapper;
+
+        [TestInitialize]
+        public virtual void BaseInitialize()
+        {
+            MapperConfiguration configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MapperProfile());
+            });
+            _mapper = configuration.CreateMapper();
+        }
+    }
+}
